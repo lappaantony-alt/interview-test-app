2190f7c9880a8f4bc4b08d268a2b3274379b188d
antonlappa <lappaantony@gmail.com>
2026-02-19 10:46:46 +0100

    update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quiz-v1';
+const CACHE_NAME = 'quiz-v2';
 const ASSETS = [
     '/',
     '/index.html',
@@ -7,12 +7,29 @@ const ASSETS = [
     '/data_part1.js',
     '/data_part2.js',
     '/data_part3.js',
+    '/data_en.js',
+    '/data_de.js',
     '/manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+// Cache cleanup
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
     );
 });
 
